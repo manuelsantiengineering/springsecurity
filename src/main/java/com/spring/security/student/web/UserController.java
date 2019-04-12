31f8bdec16eb091258baece20da727ab5f6f0498
Manuel Santiago <manuelsantiengineering@gmail.com>
2019-04-12 11:51:02 -0400

    Created the user controller

--- src/main/java/com/spring/security/student/web/UserController.java
+++ src/main/java/com/spring/security/student/web/UserController.java
@@ -2,10 +2,13 @@ package com.spring.security.student.web;
 
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.security.student.services.UserService;
@@ -23,5 +26,14 @@ public class UserController {
 	@GetMapping(value="/all-users",produces="application/json")
 	public Mono<Collection<String>> getAllUsers() {
 		return Mono.just(userService.getAllUsers());
+	}
+	
+	@GetMapping(value="/user/{userId}",produces="application/json")
+	public Mono<String> getSpecificUser(@PathVariable("userId") Integer userId, HttpServletRequest httpServletRequest) {
+		if(httpServletRequest.isUserInRole("ROLE_USER")) {
+			return Mono.justOrEmpty(userService.getById(userId));
+		}else {
+			return Mono.error(new Exception("Unauthorized user."));
+		}
 	}
 }
